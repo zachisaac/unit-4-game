caadf0ea726302c18673166482f9a1c883d7058c
zpaki5142 <zachisaac@users.noreply.github.com>
2018-08-23 14:20:54 -0700

    Second Commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,25 @@
+//Game  characters
+var game = {
+    characters: {
+        'Obi-Wan': {
+            image: 'obi-wan-kenobi.jpeg',
+            baseAttackPower: 8,
+            healthPoint: 120,
+        },
+        'Luke Skywalker': {
+            image: 'luke-skywalker.jpg',
+            baseAttackPower: 5,
+            healthPoint: 100,
+        },
+        'Darth Vader': {
+            image: 'darth-sidious.jpeg',
+            baseAttackPower: 20,
+            healthPoint: 150,
+        },
+        'Yoda': {
+            image: 'darth-maul.jpeg',
+            baseAttackPower: 25,
+            healthPoint: 180,
+        },
+    }
+};
